4a776fe8aed89107b2cdf223c42966845f9e286d
Alexey Pochekutov <alexeyerpd@gmail.com>
2022-11-21 17:34:53 +0300

    fix lint

--- src/__tests__/fetchData.test.js
+++ src/__tests__/fetchData.test.js
@@ -2,8 +2,8 @@ import fetchData from "../http";
 
 test("should be error", () => {
     try {
-        fetchData();
+        fetchData(1);
     } catch (e) {
-        expect(e.message).toBe("Mock this!");
+        expect(e.message).toBe("Mock this! 1");
     }
 });
